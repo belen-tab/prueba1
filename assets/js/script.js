eb758203560acc2606ba7d8ea0f73186f482e00f
Belén <belen.tamblay@mail.udp.cl>
2021-05-30 21:09:25 -0400

    nueva seccion y cambio de carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,5 +14,55 @@ $(function(){
         }
       });
 
+      $(".imagen-trucha").mouseover(function(){
+        $(".texto-trucha").show();
+      })
+
+      $(".imagen-trucha").mouseout(function(){
+        $(".texto-trucha").hide();
+      })
+
+      $(".imagen-benat").mouseover(function(){
+        $(".texto-benat").show();
+      })
+
+      $(".imagen-benat").mouseout(function(){
+        $(".texto-benat").hide();
+      })
+
+      $(".imagen-cabezona").mouseover(function(){
+        $(".texto-cabezona").show();
+      })
+
+      $(".imagen-cabezona").mouseout(function(){
+        $(".texto-cabezona").hide();
+      })
+
+      $(".imagen-js").mouseover(function(){
+        $(".texto-js").show();
+      })
+
+      $(".imagen-js").mouseout(function(){
+        $(".texto-js").hide();
+      })
+
+      $(".imagen-met").mouseover(function(){
+        $(".texto-met").show();
+      })
+
+      $(".imagen-met").mouseout(function(){
+        $(".texto-met").hide();
+      })
+
+      $(".imagen-elcira").mouseover(function(){
+        $(".texto-elcira").show();
+      })
+
+      $(".imagen-elcira").mouseout(function(){
+        $(".texto-elcira").hide();
+      });
+
+      
+
       
 });
